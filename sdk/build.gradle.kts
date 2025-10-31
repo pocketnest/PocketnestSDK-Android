d847a8c22c0de23770fbf5421dfd19efb0af181a
Andrey Molchanovskiy <a.molchanovskiy@gmail.com>
2025-10-31 11:02:12 +0200

    update publishing

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -34,59 +34,21 @@ android {
     }
 }
 
-afterEvaluate {
-    val pub = publishing.publications.create("release", MavenPublication::class.java) {
-        from(components["release"])
-        groupId = project.group.toString()
-        artifactId = (findProperty("POM_ARTIFACT_ID") as String?) ?: "pocketnest-sdk"
-        version = project.version.toString()
 
-        pom {
-            name.set(findProperty("POM_NAME") as String? ?: "Pocketnest SDK (Android)")
-            description.set(findProperty("POM_DESCRIPTION") as String? ?: "Android SDK for Pocketnest")
-            url.set(findProperty("POM_URL") as String? ?: "https://github.com/pocketnest")
+// 💡 In Kotlin DSL you don't need afterEvaluate here.
+// Configure a Maven publication that JitPack will pick up.
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            // JitPack expects this groupId format
+            groupId = "com.github.pocketnest"
+            // This is what your clients will use as artifactId
+            artifactId = "PocketnestSDK-Android"
+            // The version comes from the Git tag (e.g., v1.0.1) when JitPack builds
 
-            licenses {
-                license {
-                    name.set(findProperty("POM_LICENSE_NAME") as String? ?: "The MIT License")
-                    url.set(findProperty("POM_LICENSE_URL") as String? ?: "https://opensource.org/licenses/MIT")
-                    distribution.set(findProperty("POM_LICENSE_DIST") as String? ?: "repo")
-                }
-            }
-            scm {
-                url.set(findProperty("POM_SCM_URL") as String? ?: "https://github.com/pocketnest/pocketnest-android")
-                connection.set(findProperty("POM_SCM_CONNECTION") as String? ?: "scm:git:git://github.com/pocketnest/pocketnest-android.git")
-                developerConnection.set(findProperty("POM_SCM_DEV_CONNECTION") as String? ?: "scm:git:ssh://git@github.com/pocketnest/pocketnest-android.git")
-            }
-            developers {
-                developer {
-                    id.set(findProperty("POM_DEVELOPER_ID") as String? ?: "pocketnest")
-                    name.set(findProperty("POM_DEVELOPER_NAME") as String? ?: "Pocketnest")
-                }
-            }
+            // Publish the Android "release" AAR
+            from(components["release"])
         }
-    }
-
-    publishing.repositories.maven {
-        name = "CentralPortal"
-        url = uri(
-            if (version.toString().endsWith("SNAPSHOT"))
-                "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-            else
-                "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-        )
-        credentials {
-            username = findProperty("centralUsername") as String? ?: System.getenv("CENTRAL_USERNAME")
-            password = findProperty("centralPassword") as String? ?: System.getenv("CENTRAL_PASSWORD")
-        }
-    }
-
-    signing {
-        useInMemoryPgpKeys(
-            findProperty("signingInMemoryKey") as String?,
-            findProperty("signingInMemoryKeyPassword") as String?
-        )
-        sign(pub)
     }
 }
 
